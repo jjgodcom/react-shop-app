25e0b00ef9e6d69f31a1701a1a8b564e6b100340
jjgodcom <jjgodcom1@gmail.com>
2023-09-25 01:30:57 +0900

    카테고리 컴포넌트 생성

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import FiltersCategory from './filter-category/FiltersCategory'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div className='page'>
+      <div className='container'>
+        <h1>Products</h1>
+        <FiltersCategory></FiltersCategory>
+      </div>
+    </div>
   )
 }
 
---
+++ src/store/categories/categories.type.js
@@ -0,0 +1,7 @@
+export const CategoriesName = {
+  All: "",
+  Electrinics: "Electrinics",
+  Jewelry: "Jewelry",
+  MensClothing: `Men's clothing`,
+  WomensClothing: `Women's clothing`
+}
